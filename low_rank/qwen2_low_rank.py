458122dfb992728ac15fde92200dd22df103cae7
RajShah-1 <srk13@iitbbs.ac.in>
2024-11-24 17:25:14 -0500

    some wip on qwen2 low_rank; getting OOM on L40s

--- low_rank/qwen2_low_rank.py
+++ low_rank/qwen2_low_rank.py
@@ -4,6 +4,7 @@ from torch import nn
 import torch.nn.utils.prune as prune
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoProcessor, Trainer, TrainingArguments
+from transformers import Qwen2VLProcessor
 from qwen_vl_utils import process_vision_info
 
 from model.utils import setup_cache_dir
@@ -65,10 +66,43 @@ SKIP_LAYERS = ['model.layers.20.mlp.gate_proj',
 'model.layers.27.mlp.down_proj',
 'lm_head']
 
+
+def clear_memory():
+    import gc
+    import time
+    # Delete variables if they exist in the current global scope
+    # if "inputs" in globals():
+    #     del globals()["inputs"]
+    # if "model" in globals():
+    #     del globals()["model"]
+    # if "processor" in globals():
+    #     del globals()["processor"]
+    # if "trainer" in globals():
+    #     del globals()["trainer"]
+    # if "peft_model" in globals():
+    #     del globals()["peft_model"]
+    # if "bnb_config" in globals():
+    #     del globals()["bnb_config"]
+    # time.sleep(2)
+
+    # Garbage collection and clearing CUDA memory
+    gc.collect()
+    time.sleep(2)
+    torch.cuda.empty_cache()
+    torch.cuda.synchronize()
+    time.sleep(2)
+    gc.collect()
+    time.sleep(2)
+
+    print(f"GPU allocated memory: {torch.cuda.memory_allocated() / 1024**3:.2f} GB")
+    print(f"GPU reserved memory: {torch.cuda.memory_reserved() / 1024**3:.2f} GB")
+
+
 class LowRankLinear(nn.Module):
-    def __init__(self, in_features, out_features, rank_ratio):
+    def __init__(self, in_features, out_features, rank):
         super(LowRankLinear, self).__init__()
-        self.rank = max(1, int(rank_ratio * min(in_features, out_features)))
+        self.rank = max(1, rank)
+        print('init with rank', self.rank)
         self.in_features = in_features
         self.out_features = out_features
         
@@ -87,7 +121,7 @@ class LowRankLinear(nn.Module):
         return self.linear2.weight.data @ self.linear1.weight.data
     
     @classmethod
-    def from_linear(cls, linear_layer, rank_ratio):
+    def from_linear(cls, linear_layer, retained_variance):
         """
         Initialize from an existing nn.Linear layer using SVD decomposition
         with proper dimension handling
@@ -95,20 +129,23 @@ class LowRankLinear(nn.Module):
         device = linear_layer.weight.device
         in_features = linear_layer.in_features
         out_features = linear_layer.out_features
-        
-        instance = cls(in_features, out_features, rank_ratio)
-        instance = instance.to(device)
-        rank     = instance.rank
-
         # Init low rank layers using SVD
         with torch.no_grad():
             try:
                 U, S, V = torch.svd(linear_layer.weight.data)
-                sqrt_s = torch.sqrt(S[:rank]).view(-1, 1)
-                
+                total_variance = torch.sum(S ** 2)
+                cumulative_variance = torch.cumsum(S ** 2, dim=0)
+                rank = torch.searchsorted(cumulative_variance, retained_variance * total_variance).item()
+                # Ensure minimum rank of 1
+                # init instance
+                instance = cls(in_features, out_features, rank)
+                instance = instance.to(device)
+                rank = instance.rank
+            
                 # Expected dimensions:
                 # linear1.weight -> (rank, in_features)
                 # linear2.weight -> (out_features, rank)
+                sqrt_s = torch.sqrt(S[:rank]).view(-1, 1)
                 instance.linear1.weight.data = (V[:, :rank] * sqrt_s.T).T
                 instance.linear2.weight.data = U[:, :rank] * sqrt_s.T
                 
@@ -134,7 +171,7 @@ class LowRankLinear(nn.Module):
         """
         Return compression statistics
         """
-        original_params = self.in_features * self.out_features + self.out_features
+        original_params   = self.in_features * self.out_features + self.out_features
         compressed_params = (self.in_features * self.rank +    # first layer weights
                              self.rank * self.out_features +   # second layer weights
                              self.out_features)                # bias
@@ -147,7 +184,7 @@ class LowRankLinear(nn.Module):
         }
         return stats
 
-def replace_linear_with_low_rank(model, rank_ratio, skip_patterns=None):
+def replace_linear_with_low_rank(model, retained_variance, skip_patterns=None):
     """
     Replace linear layers with low-rank versions initialized using SVD
     """
@@ -167,7 +204,7 @@ def replace_linear_with_low_rank(model, rank_ratio, skip_patterns=None):
                 print(f"\nProcessing layer {name}")
                 print(f"Original shape: {module.weight.shape}")
                 
-                low_rank_module = LowRankLinear.from_linear(module, rank_ratio)                
+                low_rank_module = LowRankLinear.from_linear(module, retained_variance)                
                 print(f"Low rank shapes: {low_rank_module.linear1.weight.shape} -> {low_rank_module.linear2.weight.shape}")
                 
                 # Compression stats
@@ -189,8 +226,8 @@ def replace_linear_with_low_rank(model, rank_ratio, skip_patterns=None):
                     U, S, V = torch.svd(module.weight.data)
                     rank = low_rank_module.rank
                     total_variance = torch.sum(S ** 2)
-                    retained_variance = torch.sum(S[:rank] ** 2)
-                    variance_ratio = retained_variance / total_variance
+                    module_retained_var = torch.sum(S[:rank] ** 2)
+                    variance_ratio = module_retained_var / total_variance
                     print(f"Layer {name}: Retaining {variance_ratio:.2%} of variance")
                     
             except Exception as e:
@@ -217,50 +254,73 @@ def create_dataset():
 
     return train_dataset, eval_dataset
 
+IGNORE_INDEX = -100
+DEFAULT_IM_START_TOKEN = "<|im_start|>"
+DEFAULT_IM_END_TOKEN = "<|im_end|>"
+DEFAULT_IMAGE_TOKEN = "<|image_pad|>"
+DEFAULT_VIDEO_TOKEN = "<|video_pad|>"
+LLAVA_IMAGE_TOKEN = "<image>"
+LLAVA_VIDEO_TOKEN = "<video>"
+VISION_START_TOKEN = "<|vision_start|>"
+VISION_END_TOKEN = "<|vision_end|>"
+
+SYSTEM_MESSAGE = "You are a helpful assistant."
+
+def format_data(sample):
+    return [
+        {
+            "role": "system",
+            "content": [{"type": "text", "text": SYSTEM_MESSAGE}],
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": sample["image"],
+                },
+                {
+                    "type": "text",
+                    "text": sample["query"],
+                },
+            ],
+        },
+        {
+            "role": "assistant",
+            "content": [{"type": "text", "text": random.choice(sample["answers"])}],
+        },
+    ]
+
+
 class MiniDocVQADataCollator:
     def __init__(self, processor):
         self.processor = processor
 
     def __call__(self, examples):
-        assert len(examples) == 1, 'Batch size must be 1!'
-        example = examples[0]
+        print('Batch Size: ', len(examples))
+        # assert len(examples) == 1, 'Batch size must be 1!'
+        examples = [format_data(example) for example in examples] 
+        image_inputs = [process_vision_info(example)[0] for example in examples]
+        texts = [self.processor.apply_chat_template(example, tokenize=False) for example in examples]
 
-        image = example['image']
-        question = example['query']['en']
-        answer = random.choice(example['answers'])
-        prompt_message = {
-            'role': 'user',
-            'content': f'<|image_1|>\n{question}\nAnswer briefly.',
-        }
-        
-        # Qwen specific step
-        image_inputs, video_inputs = process_vision_info([prompt_message])
+        batch = self.processor(text=texts, images=image_inputs, return_tensors='pt', padding=True)
 
-        prompt = self.processor.tokenizer.apply_chat_template(
-            [prompt_message], tokenize=False, add_generation_prompt=True
-        )
-        answer = f'{answer}<|end|>\n<|endoftext|>'
+        # The labels are the input_ids, and we mask the padding tokens in the loss computation
+        labels = batch["input_ids"].clone()  # Clone input IDs for labels
+        labels[labels == self.processor.tokenizer.pad_token_id] = -100  # Mask padding tokens in labels
 
-        # Process input and labels
-        batch = self.processor(text=prompt, images=image_inputs, videos=video_inputs, return_tensors='pt', padding=True)
-        prompt_input_ids = batch['input_ids']
-        answer_input_ids = self.processor.tokenizer(
-            answer, add_special_tokens=False, return_tensors='pt'
-        )['input_ids']
-        input_ids = torch.cat([prompt_input_ids, answer_input_ids], dim=1)
+        # Ignore the image token index in the loss computation (model specific)
+        if isinstance(self.processor, Qwen2VLProcessor):  # Check if the processor is Qwen2VLProcessor
+            # print('NOTE: This is Qwen2VLProcessor!')
+            image_tokens = [151652, 151653, 151655]  # Specific image token IDs for Qwen2VLProcessor
+        else:
+            image_tokens = [self.processor.tokenizer.convert_tokens_to_ids(self.processor.image_token)]  # Convert image token to ID
 
-        ignore_index = -100
-        labels = torch.cat(
-            [
-                torch.tensor([ignore_index] * len(prompt_input_ids[0])).unsqueeze(0),
-                answer_input_ids,
-            ],
-            dim=1,
-        )
+        # Mask image token IDs in the labels
+        for image_token_id in image_tokens:
+            labels[labels == image_token_id] = -100  # Mask image token IDs in labels
 
-        batch['input_ids'] = input_ids
-        del batch['attention_mask']
-        batch['labels'] = labels
+        batch["labels"] = labels  # Add labels to the batch
 
         return batch
 
@@ -293,6 +353,30 @@ def get_model_size(model):
     return size_all_mb
 
 
+def prune_model_layers(model, keep_layers):
+    """
+    Prune transformer layers in the model.
+    
+    Args:
+        model: The transformer model.
+        keep_layers: List of layer indices to keep.
+        
+    Returns:
+        The pruned model.
+    """
+    # Access the model's layers
+    transformer_layers = model.model.layers
+
+    # Prune layers
+    pruned_layers = torch.nn.ModuleList(
+        [transformer_layers[i] for i in keep_layers]
+    )
+    
+    # Replace the model's layers with pruned layers
+    model.model.layers = pruned_layers
+
+    return model
+
 def main():
     model_name = "Qwen/Qwen2-VL-2B-Instruct"
     cache_dir = setup_cache_dir()
@@ -309,13 +393,21 @@ def main():
     # Actual pruning code would come here
     # ===
     print("Original model size:", get_model_size(model))
+
+    num_layers = len(model.model.layers)  # Total number of layers
+    keep_layers = list(range(0, num_layers, 2))  # Keep every alternate layer
+    print(f"Original number of layers: {num_layers}")
+    model = prune_model_layers(model, keep_layers)
+    print(f"Pruned number of layers: {len(model.model.layers)}")
+
+    model.gradient_checkpointing_enable()
     
     # Replace with low-rank layers, using SVD initialization
-    model = replace_linear_with_low_rank(
-        model, 
-        rank_ratio=0.5,
-        skip_patterns=SKIP_LAYERS
-    )
+    # model = replace_linear_with_low_rank(
+    #     model, 
+    #     retained_variance=0.95,
+    #     skip_patterns=SKIP_LAYERS
+    # )
     
     print("Compressed model size:", get_model_size(model))
     # ===
@@ -327,25 +419,26 @@ def main():
     # Define training arguments
     training_args = TrainingArguments(
         num_train_epochs=2,
-        per_device_train_batch_size=1,
+        per_device_train_batch_size=50,
         gradient_checkpointing=False,
         optim='adamw_torch',
         learning_rate=4e-5,
         weight_decay=0.01,
-        max_grad_norm=1.0,
+        max_grad_norm=0.5,
         lr_scheduler_type='linear',
         warmup_steps=50,
         logging_steps=10,
         output_dir=output_dir,
-        bf16=True,
+        bf16=True,  # Use bfloat16 for better memory efficiency
+        fp16=False, # Disable fp16 when using bf16
         remove_unused_columns=False,
-        dataloader_num_workers=4,
-        dataloader_prefetch_factor=2,
+        dataloader_num_workers=0,
+        dataloader_prefetch_factor=None,
         save_strategy="no",
     )
 
     data_collator = MiniDocVQADataCollator(processor)
-
+    model.config.use_cache = False
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -355,6 +448,9 @@ def main():
 
     # Fine-tune the model
     print("\nStarting fine-tuning...\n")
+
+    clear_memory()
+
     trainer.train()
     print("\nFine-tuning completed.")
 
@@ -365,8 +461,8 @@ def main():
     print("Model saved successfully.")
 
     print("Saving processor")
-    if not hasattr(processor, 'chat_template'):
-        processor.chat_template = None
+    # if not hasattr(processor, 'chat_template'):
+    #     processor.chat_template = None
 
     print("Saving processor...")
     processor.save_pretrained(output_dir)
